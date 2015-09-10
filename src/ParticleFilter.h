75cb55b48341d6a731d876e1a416401c673aa2d9
Jeremy Nicola <jeremy.nicola@gmail.com>
2015-09-10 14:41:03 +0200

    Base compilable library

---
+++ src/ParticleFilter.h
@@ -0,0 +1,14 @@
+#ifndef PARTICLE_FILTER_H
+#ifndef PARTICLE_FILTER_H
+
+#DEFINE PARTICLE_NUMBER // Number of particles
+
+#include <Eigen/Dense>
+
+class ParticleFilter{
+private:
+
+public:
+}
+
+#endif // PARTICLE_FILTER_H
